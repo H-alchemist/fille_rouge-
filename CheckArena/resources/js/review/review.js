68c8a7e88baaf7cebe3b1bd8dcab8cb26db9b706
hamza <echchablihamza1@gmail.com>
2025-05-01 18:42:16 +0100

    add logic to manipulate the board and display the board position

--- CheckArena/resources/js/review/review.js
+++ CheckArena/resources/js/review/review.js
@@ -1,8 +1,34 @@
  
- console.log(moves);
- 
-    document.addEventListener('DOMContentLoaded', function() {
-        
+//  console.log(moves);
+
+
+ let defaultPosition = [
+     [-2, -3, -4, -5, -6, -4, -3, -2],
+     [-1, -1, -1, -1, -1, -1, -1, -1],
+     [0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0],
+     [1, 1, 1, 1, 1, 1, 1, 1],
+     [2, 3, 4, 5, 6, 4, 0, 2]
+    ];
+    
+
+
+    let records = [
+
+        {
+            id: 0,
+            board:defaultPosition,
+
+        }
+    ];
+
+
+    let moveNum = 0
+
+
+
         const pieceMap = {
             '1': 'https://assets-themes.chess.com/image/ejgfv/150/wp.png',
             '2': 'https://assets-themes.chess.com/image/ejgfv/150/wr.png',
@@ -17,17 +43,125 @@
             '-5': 'https://assets-themes.chess.com/image/ejgfv/150/bq.png',
             '-6': 'https://assets-themes.chess.com/image/ejgfv/150/bk.png'
         };
+ 
+    document.addEventListener('DOMContentLoaded', function() {
+       
         
-        
-        let defaultPosition = [
-            [-2, -3, -4, -5, -6, -4, -3, -2],
-            [-1, -1, 0, -1, -1, -1, -1, -1],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, -1, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0, 3, 0, 0],
-            [1, 1, 1, 0, 0, 1, 1, 1],
-            [2, 3, 4, 5, 6, 4, 0, 2]
-        ];
+        createRecord(JSON.parse(moves));
+
+        renderBoard(records[0].board);
+
         
 })
+
+
+
+
+
+function createRecord(liste) {
+
+ 
+
+    
+    
+    liste.forEach((move, index) => {
+
+        //  console.log(move);
+
+         let newBoard = deepCopyBoard(records[index].board);
+
+
+
+         
+         
+
+         newBoard[move.from_position[0]][move.from_position[2]] = 0;
+        // console.log(move.piece_number);
+        
+        newBoard[move.to_position[0]][move.to_position[2]] = move.piece_number;
+        // console.log(move.to_position[0] , move.to_position[2]);
+
+        // console.log(newBoard);
+          
+
+        records.push({
+            id: index+1 ,
+            board:  newBoard,
+
+        })
+
+           return ; 
+
+
+    
+    });
+
+    // console.log(records);
+    
+}
+
+let playerColor = "white";
+
+
+function renderBoard(board) {
+    
+    const boardElement = document.getElementById('chessboard');
+    
+    boardElement.innerHTML = '';
+    
+    // boardElement.innerHTML = playerColor=== "white" ? whiteBoard : blackBoard;
+
+
+    
+    for (let row = 7; row >= 0; row--) {
+        for (let col = 7; col >= 0; col--) {
+            const sq = document.createElement('div');
+            sq.classList.add('pieceContainer');
+            
+            // Adjust grid positions
+            const gridRow = playerColor === "white" ? row + 1 : 7 - row + 1;
+            const gridCol = playerColor === "white" ? col + 1 : 7 - col + 1;
+            sq.style.gridColumn = gridCol;
+            sq.style.gridRow = gridRow;
+           
+           
+            
+            const pieceNum = board[row][col];
+            if (pieceNum !== 0) {
+                const piece = pieceMap[pieceNum]; 
+                const img = document.createElement('img');
+                img.src = piece;
+               
+                img.classList.add('piece');
+                sq.appendChild(img);
+            }
+            
+            boardElement.appendChild(sq);
+        }
+    }
+    
+    
+   
+}
+
+
+function deepCopyBoard(board) {
+    return JSON.parse(JSON.stringify(board));
+}
+
+
+document.getElementById('next-move').addEventListener(()=>{
+
+
+
+    moveNum++;
+    renderBoard(records[moveNum].board);
+
+
+
+
+
+
+
+
+});
---
+++ CheckArena/resources/js/review/review.md
@@ -0,0 +1,6 @@
+
+
+# there is problem in the review page the user should pass to the next move and return to the previous 
+# and the data i have is not enough to do that because i dont have screenshot of the board at every move and i dont have record in case of captured pieace 
+# so my idea is take the array of object and create record of the board at any move  
+# pass the board to render board 
